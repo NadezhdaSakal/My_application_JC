3cb5b2501ecbfa7636019204d31d7f3da876219f
NadezhdaSakal <sakaln88@gmail.com>
2025-01-20 23:02:07 +0300

    clean MainActivity

--- app/src/main/java/com/sakal_n/myapplication_jc/MainActivity.kt
+++ app/src/main/java/com/sakal_n/myapplication_jc/MainActivity.kt
@@ -16,32 +16,8 @@ import com.sakal_n.myapplication_jc.ui.theme.MyApplication_JCTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            MyApplication_JCTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplication_JCTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/sakal_n/myapplication_jc/ui/theme/InstaProfileCard.kt
@@ -0,0 +1,2 @@
+package com.sakal_n.myapplication_jc.ui.theme
+
